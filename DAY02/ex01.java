a48bb61b13cdd8b354c0960c0938e4bdff53aace
halloumi achouak <a.halloumi@it-students.fr>
2021-09-15 16:35:41 +0200

    dayJAVA

---
+++ DAY02/ex01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+import java.lang.* ; 
+import java.util.*;  
+class Main {  
+
+	
+  public static void main(String args[]) { 
+   
+  Scanner x = new Scanner(System.in);
+  System.out.println("Entrer une chaine    "); 
+	 String ent = x.nextLine () ; 
+ 
+	  String conver = ent.toLowerCase();
+	 System.out.println(conver);
+}
+}
---
+++ DAY02/ex02.java
@@ -0,0 +1,46 @@
+import java.util.Scanner ;
+import java.lang.* ; 
+import java.util.*;  
+
+class Main {  
+
+	
+  public static void main(String args[]) { 
+   
+  Scanner x = new Scanner(System.in);
+  System.out.println("entrer un nombre romain "); 
+ String ent = x.nextLine () ; 
+int valeur=0 ; 
+	//String ent = "MDC"  ;
+	
+	  int N = ent.length();
+
+for (int i=0 ; i < N ; i++ ){
+  char y = ent.charAt(i);
+	switch(y){
+			case('M'):
+			valeur = valeur + 1000 ; 
+			break;
+			case('D'):
+			valeur = valeur + 500 ; 
+			break;
+			case('C'):
+			valeur = valeur + 100 ; 
+			break;
+			case('L'):
+			valeur = valeur + 50 ; 
+			break;
+			case('X'):
+			valeur = valeur + 10 ; 
+			break;
+			case('V'):
+			valeur = valeur + 5 ; 
+			break;
+			case('I'):
+			valeur = valeur + 1 ; 
+			break;
+	}
+
+	}System.out.println(valeur); 
+}
+}
